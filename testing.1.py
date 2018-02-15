81b6e756a516329c916aa22341648a1ee47eb1f5
Amanda Perez <amandaparaperez@gmail.com>
2018-02-14 22:47:38 -0700

    Python Demo Code -1

---
+++ testing.1.py
@@ -0,0 +1,57 @@
+Python 3.6.2 (v3.6.2:5fd33b5926, Jul 16 2017, 20:11:06) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+python
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    python
+NameError: name 'python' is not defined
+>>> print("Hello World")
+Hello World
+>>> print("My name is Amanda")
+My name is Amanda
+>>> 7 + 2
+9
+>>> #strings
+>>> "Hello there " + "I am Amanda " + "and I am pleased to meet you"
+'Hello there I am Amanda and I am pleased to meet you'
+>>> #muliplying strings
+>>> "Amanda loves her dog, tabitha" * 7
+'Amanda loves her dog, tabithaAmanda loves her dog, tabithaAmanda loves her dog, tabithaAmanda loves her dog, tabithaAmanda loves her dog, tabithaAmanda loves her dog, tabithaAmanda loves her dog, tabitha'
+>>> #uppercase letters
+>>> "Amanda".upper()
+'AMANDA'
+>>> #how many letters do we have?
+>>> len(Amandaisgratefulforherfather)
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    len(Amandaisgratefulforherfather)
+NameError: name 'Amandaisgratefulforherfather' is not defined
+>>> len("AmadnaIsGratefulForHerFather")
+28
+>>> #length size of a list
+>>> family = [ 'grandma', 'grandpa', 'sharlet', 'father', 'mother', 'aunt' ]
+>>> len(family)
+6
+>>> #storing data in a variable
+>>> father = "Henry"
+>>> father
+'Henry'
+>>> #lets compare
+>>> 10 > 2
+True
+>>> 20 < 5
+False
+>>> 6 > 2 * 4
+False
+>>> 7 == 7
+True
+>>> 5 ! = 2
+SyntaxError: invalid syntax
+>>> 5 ! = 2
+SyntaxError: invalid syntax
+>>> 5!=2
+True
+>>> 
